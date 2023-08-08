b3230705abff97c98677e4c44a3d4679b087fc25
dun <DundeeDevelops@gmail.com>
2023-08-08 00:21:30 -0500

    Implemented ThreeJS with cube

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,62 @@
 import './App.css'
+import { useEffect } from 'react'
+import * as THREE from 'three' 
+
+function App(){ 
+
+ 
+
+  useEffect(() => {
+   
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+     50,
+     window.innerWidth / window.innerHeight,
+     1,
+     1000
+    );
+    camera.position.z = 96;
+
+    const canvas = document.getElementById('myThreeJsCanvas')
+    const renderer = new THREE.WebGLRenderer({
+      canvas,
+      antialias: true
+    });
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
 
 
-function App(){
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+    ambientLight.castShadow = true;
+    scene.add(ambientLight);
+
+
+    const spotLight = new THREE.SpotLight(0xffffff,1);
+    spotLight.castShadow = true;
+    spotLight.position.set(0,64,32);
+    scene.add(spotLight);
+     
+    const boxG = new THREE.BoxGeometry(16,16,16);
+    const boxMat = new THREE.MeshNormalMaterial();
+    const boxMesh = new THREE.Mesh(boxG, boxMat);
+    scene.add(boxMesh);
+
+    const animate = () => {
+      boxMesh.rotation.x += 0.002;
+      boxMesh.rotation.y += 0.002; 
+      renderer.render(scene,camera);
+      window.requestAnimationFrame(animate);
+    };
+ 
+    animate(); 
+  },[])
+
+
   return(
-   <div id='app'>
-   <h1>ThreeJS Testing!</h1>
+   <div id='app'> 
+    <h1>Three JS</h1>
+   <canvas id='myThreeJsCanvas'/>
    </div>
   )
 } 
