f041b77110b2ce0fd2f2daef905d58c6dfeb77c6
dun <DundeeDevelops@gmail.com>
2023-08-11 18:49:52 -0500

    Switched to react-three fiber

--- src/App.tsx
+++ src/App.tsx
@@ -1,86 +1,30 @@
 import './App.css'
-import { useEffect } from 'react'
-import * as THREE from 'three'
 
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
-function App() {
-  useEffect(() => {
-    //initalize scene
-    const scene = new THREE.Scene()
+import { Canvas, useFrame } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei"; 
+import { useMemo, useRef } from "react";
 
-    //create camera
-    const camera = new THREE.PerspectiveCamera(
-      90,
-      window.innerWidth / window.innerHeight,
-      1,
-      1000,
-    )
-    //position camera
-    camera.position.z = 3
-    camera.position.y = 2
-
-    const canvas = document.getElementById('myThreeJsCanvas')
-
-    //create renderer
-    const renderer = new THREE.WebGLRenderer({
-      canvas,
-      antialias: true,
-      alpha: true,
-    })
-
-    //canvas size
-    renderer.setSize(window.innerWidth, window.innerHeight)
-
-    document.body.appendChild(renderer.domElement)
-
-    //lighting
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
-    ambientLight.castShadow = true
-    scene.add(ambientLight)
-
-    const spotLight = new THREE.SpotLight(0xffffff, 1)
-    spotLight.castShadow = true
-    spotLight.position.set(0, 64, 32)
-    scene.add(spotLight)
-
-    const loader = new GLTFLoader()
-
-    const textureLoader = new THREE.TextureLoader()
-    const alphaMap = textureLoader.load('./src/assets/opacitymap.png')
-
-    let earth1 = null
-
-    loader.load(
-      './src/assets/earth101.glb',
-      function (gltf: any) {
-        scene.add(gltf.scene)
-        earth1 = gltf.scene
-
-        gltf.scene.children[0].material.alphaMap = alphaMap
-
-        console.log(gltf.scene.children[0].material)
-      },
-      undefined,
-      function (error: any) {
-        console.error(error)
-      },
-    )
-
-    function animate() {
-      requestAnimationFrame(animate)
-      renderer.render(scene, camera)
-
-      earth1.rotation.y += 0.002
-    }
-
-    animate()
-  }, [])
-
+const BasicParticles = () => {
+  // This reference gives us direct access to our points
+  const points = useRef<THREE.Points>(null!);
+ 
+  // You can see that, like our mesh, points also takes a geometry and a material,
+  // but a specific material => pointsMaterial
   return (
-    <div id="app">
-      <h1>Three JS</h1>
-      <canvas id="myThreeJsCanvas" />
-    </div>
+    <points ref={points}>
+      <sphereGeometry args={[1, 48, 40]} />
+      <pointsMaterial color="#5786F5" size={0.015} sizeAttenuation />
+    </points> 
+  ); 
+};
+
+function App() {  
+  return ( 
+    <Canvas camera={{ position: [0.6, 2, 0.6] }}>
+    <ambientLight intensity={0.5} />
+    <BasicParticles />
+    <OrbitControls autoRotate />
+    </Canvas>
   )
 }
 
